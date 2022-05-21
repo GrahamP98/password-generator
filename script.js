9880379b250a2356f0a29dc50e4eec3bbd7213e2
GrahamP98 <g.purnell15@gmail.com>
2022-05-21 13:28:13 -0400

    uploaded starter code and started .js file

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-.
+// Assignment code here
+var lowercaseletters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var special = "<>,./?'[{]}|=+-_)(*&^%$#@!~`"
+var nums = "0123456789"
+var numscheck;
+var specialcheck;
+var uppercaseletterscheck;
+var password length;
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
