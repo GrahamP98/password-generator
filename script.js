3d1c4c7f1af2eaef3af8467d48ac417c9cbd9c59
GrahamP98 <g.purnell15@gmail.com>
2022-05-21 15:30:50 -0400

    inputed character selector

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment code here
+// Assignment code here.
 var lowercaseletters = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var special = "<>,./?'[{]}|=+-_)(*&^%$#@!~`";
@@ -9,10 +9,10 @@ var uppercaselettersverify;
 var passwordlength;
 
 
-// Get references to the #generate element
+// Get references to the #generate element.
 var generateBtn = document.querySelector("#generate");
 
-// Function for figuring out password size
+// Function for figuring out password size.
 function passwordsize() {
     passwordlength = prompt("How many characters would you like in your password? (must be at least 8 characters but no more than 128.)");
 
@@ -25,21 +25,56 @@ function passwordsize() {
         passwordsize();
     }
     else {
-        alert("....")
+        alert("You will be asked what you want included in your password. \n `No` input for the next prompts will issue a lowercase letter password.");
     }
     return passwordlength;
 }
-
+// Function for figuring out if user wants number(s) in password.
 function numsverification(){
     numsverify = prompt("Do you want to use numbers in your password? \n(Yes or No)");
+    numsverify = numsverify.toLowerCase();
+    
+
+
+    return numsverify;
 }
 // Generates password using criteria inputed by user. !showing as "undefined"!
 function generatePassword(){
     passwordsize();
     console.log(passwordsize);
+    numsverification();
+    console.log(numsverify);
+
+    var characters = lowercaseletters;
+    var password = "";
+    if (numsverify && specialverify && uppercaselettersverify){
+        characters += nums + special + uppercaseletters;
+    }
+    else if (numsverify && specialverify){
+        characters += nums + special;
+    }
+    else if (specialverify && uppercaselettersverify){
+        characters += special + uppercaseletters;
+    }
+    else if (numsverify && uppercaselettersverify){
+        characters += nums + uppercaseletters;
+    }
+    else if (numsverify){
+        characters += nums;
+    }
+    else if (uppercaselettersverify){
+        characters += uppercaseletters;
+    }
+    else if (specialverify){
+        characters += special;
+    }
+    else{
+        characters === lowercaseletters;
+    }
+    return password;
 }
 
-// Write password to the #password input
+// Write password to the #password input.
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
@@ -48,5 +83,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button.
 generateBtn.addEventListener("click", writePassword);
