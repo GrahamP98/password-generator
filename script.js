fad5c03cb01c9d3d3a13082ca69312086af7c78c
GrahamP98 <g.purnell15@gmail.com>
2022-05-21 14:42:08 -0400

    nums prompt not appearing after number of characters selected.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment code here
 var lowercaseletters = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var special = "<>,./?'[{]}|=+-_)(*&^%$#@!~`"
-var nums = "0123456789"
+var uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "<>,./?'[{]}|=+-_)(*&^%$#@!~`";
+var nums = "0123456789";
 var numsverify;
 var specialverify;
 var uppercaselettersverify;
@@ -14,7 +14,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Function for figuring out password size
 function passwordsize() {
-    passwordlength = prompt("How many characters would you like in your password? (must be at least 8 characters but no more than 128.)")
+    passwordlength = prompt("How many characters would you like in your password? (must be at least 8 characters but no more than 128.)");
 
     if (passwordlength < 8){
         alert("Password must have between 8-128 characters.");
@@ -30,6 +30,10 @@ function passwordsize() {
     return passwordlength;
 }
 
+function numsverification(){
+    numsverify = prompt("Do you want to use numbers in your password? \n(Yes or No)");
+}
+// Generates password using criteria inputed by user. !showing as "undefined"!
 function generatePassword(){
     passwordsize();
     console.log(passwordsize);
